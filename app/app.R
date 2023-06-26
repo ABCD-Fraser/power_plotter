8f145604d28a6ba20028d2d070493042de300256
ABCD-Fraser <alex.fraser@psy.ox.ac.uk>
2023-06-26 08:26:37 +0100

    add multi participant

--- app/app.R
+++ app/app.R
@@ -11,12 +11,14 @@ source("power_plotter.R")
   
 # User interface ----
 ui <- fluidPage(
-  titlePanel("censusVis"),
+  titlePanel("powerPlotter"),
   
   sidebarLayout(
     sidebarPanel(
-      helpText("Create demographic maps with 
-        information from the 2010 US Census."),
+      helpText("Plot power simulations for indipendent sample t-test. 
+               Change the settings below and press update to see the plot. 
+               The more simulations your are running the longer it will take to
+               display the plots."),
       
       
       
@@ -24,20 +26,24 @@ ui <- fluidPage(
                   label = "Number of simulations",
                   choices = c(1, 10,100, 1000),
                   selected = 100),
+      sliderInput("n_ppt", 
+                  label = "Number of data points in each sample group",
+                  min = 0, max = 100, value = c(10,100)),
+      
+      selectInput("n_steps", 
+                  label = "Steps between min and max sample size",
+                  choices = c(1, 2, 5, 10, 20),
+                  selected = 1),
+      
+      sliderInput("es_range", 
+                  label = "Range of effect sizes",
+                  min = 0, max = 1, value = c(0, 1)),
       
       selectInput("es_steps", 
                   label = "Steps between min and max effect size",
                   choices = c(10, 20, 30, 40),
                   selected = 10),
-      
-      sliderInput("es_range", 
-                  label = "Range of effect sizes",
-                  min = 0, max = 0.8, value = c(0, 0.8)),
 
-      sliderInput("n_ppt", 
-                  label = "Number of participants",
-                  min = 0, max = 100, value = 50),
-      
       actionButton("go", "Update")
       
     ),
@@ -52,7 +58,7 @@ server <- function(input, output) {
   
   output$map <- renderPlot({
     input$go
-    isolate(power_plot(as.numeric(input$n_ppt), as.numeric(input$es_range[1]), as.numeric(input$es_range[2]), as.numeric(input$es_steps), as.numeric(input$sim_n)))
+    isolate(power_plot(as.numeric(input$n_ppt[1]), as.numeric(input$n_ppt[2]), as.numeric(input$n_steps), as.numeric(input$es_range[1]), as.numeric(input$es_range[2]), as.numeric(input$es_steps), as.numeric(input$sim_n)))
   })
   
   
--- app/power_plotter.R
+++ app/power_plotter.R
@@ -3,9 +3,9 @@ library(tidyverse)
 library(broom)
 
 
-power_plot = function(n_per_group = 50, min_es = 0.1, max_es=0.8, es_steps=30, sim_n = 100){
+power_plot = function(min_n = 10, max_n=100, n_steps = 10, min_es = 0.0, max_es=1, es_steps=30, sim_n = 100){
   #Sample size
-  # n_per_group <- 50
+  n_per_group <- seq(from = min_n, to = max_n, length.out = n_steps) #creates sequences of numbers.
   # 
   pop_es <- seq(from = min_es, to = max_es, length.out = es_steps) #creates sequences of numbers.
   # 
@@ -14,50 +14,68 @@ power_plot = function(n_per_group = 50, min_es = 0.1, max_es=0.8, es_steps=30, s
   print('Generating plot:')
   
   powerValue <- NULL
+  overall_power = NULL
+  overall_N = NULL
+  overall_pop_es = NULL
   
-  for(es in pop_es){
+  for(N in n_per_group) {
     
-    #Container pvalues
-    pvalues <- NULL
+    powerValue=NULL
+    nValue=NULL
     
-    print(es)
-    
-    for(i in 1:sim_n){
+    print(N)
+  
+    for(es in pop_es){
+      
+      #Container pvalues
+      pvalues <- NULL
       
       
       
-      group1 <- rnorm(n = n_per_group, mean = 0, sd = 1)
-      group2 <- rnorm(n = n_per_group, mean = es, sd = 1)
+      for(i in 1:sim_n){
+        
+        
+        
+        group1 <- rnorm(n = N, mean = 0, sd = 1)
+        group2 <- rnorm(n = N, mean = es, sd = 1)
+        
+        score <- c(group1, group2)
+        groupID <- rep(c("gr1", "gr2"), each = N)
+        
+        MyData <- data.frame(groupID, score)
+        t_model <- t.test(score ~ groupID, data = MyData)
+        pvalues <- c(pvalues, tidy(t_model)$p.value[1])
+        
+      }
       
-      score <- c(group1, group2)
-      groupID <- rep(c("gr1", "gr2"), each = n_per_group)
-      
-      MyData <- data.frame(groupID, score)
-      t_model <- t.test(score ~ groupID, data = MyData)
-      pvalues <- c(pvalues, tidy(t_model)$p.value[1])
+      #Power
+      powerValue <- c(powerValue, sum(pvalues <= 0.05)/length(pvalues))
+      nValue <- c(nValue, N)
+      overall_pop_es = c(overall_pop_es, pop_es)
       
     }
     
-    #Power
-    powerValue <- c(powerValue, sum(pvalues <= 0.05)/length(pvalues))
-    
+    overall_power = c(overall_power, powerValue)
+    overall_N = c(overall_N, nValue)
+    overall_pop_es = c(overall_pop_es, pop_es)
   }
   
-  MyDataPower <- data.frame(pop_es, powerValue)
+  MyDataPower <- data.frame(overall_pop_es, overall_power, overall_N)
   
   print('finished sim')
   #Plot
-  plot = ggplot(MyDataPower, aes(x=pop_es, y=powerValue)) + 
+  plot = ggplot(MyDataPower, aes(x=overall_pop_es, y=overall_power, colour=as.factor(overall_N))) + 
     geom_line() +
     xlab("Effect Sizes")+
     ylab("Power")+
+    labs(colour = "Sample") +
     theme_bw() +
     geom_hline(yintercept= 0.8, linetype="dashed", color = "red") +
-    ggtitle("Power Curve for an Independent Sample t-test (n per group: 50)")
+    ggtitle(paste0("Power Curve for an Independent Sample t-test (n per group:",min_n, "_", max_n, ")"))
   
   plot
   
-  
+  # return(MyDataPower)
 }
 
 
